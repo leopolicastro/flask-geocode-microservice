8aee201a5308841f85df6315b6177d7e8e0fc6c1
Leo Policastro <lpolicastro@pm.me>
2021-03-09 21:03:41 -0500

    add reverse geocode route

--- index.py
+++ index.py
@@ -16,5 +16,5 @@ def hello():
 
 
 if __name__ == "__main__":
-    app.run()
-    # app.run(debug=True, port=8080)
+    # app.run()
+    app.run(debug=True, port=8080)
--- routes/geocode.py
+++ routes/geocode.py
@@ -10,9 +10,24 @@ geocode = Blueprint("geocode", __name__)
 @geocode.route("/geocode/", methods=["POST"])
 def geo():
     result = json.loads(request.data.decode("utf-8"))
-    address = result["address"]
+    try:
+        address = result["address"]
+    except:
+        return "address attribute not found"
     location = geolocator.geocode(f"{address}")
     return {
         "latitude": location.latitude,
         "longitude": location.longitude,
     }
+
+
+@geocode.route("/reverse/", methods=["POST"])
+def reverse():
+    result = json.loads(request.data.decode("utf-8"))
+    try:
+        longitude = result["longitude"]
+        latitude = result["latitude"]
+    except:
+        return "There was an error with your request"
+    location = geolocator.reverse(f"{latitude}, {longitude}")
+    return location.address
